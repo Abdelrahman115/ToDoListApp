3cceb0b02b7c37b65875f5527e42e9be13414a62
Abdelrahman <abdelrahman.tharwat95@gmail.com>
2023-05-21 17:29:04 +0300

    Initial Commit

---
+++ To Do List/To_Do_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_ListApp.swift
+//  To Do List
+//
+//  Created by Abdelrahman on 21/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
