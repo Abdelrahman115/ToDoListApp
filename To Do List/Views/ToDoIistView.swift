28d9d04c0583f5a07944c7d90b2686f5db172b33
Abdelrahman <abdelrahman.tharwat95@gmail.com>
2023-06-04 17:46:10 +0300

    configure

--- To Do List/Views/ToDoIistView.swift
+++ To Do List/Views/ToDoIistView.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct ToDoItemsView: View {
+struct ToDolistView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ToDoItemsView_Previews: PreviewProvider {
+struct ToDolistView_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoItemsView()
+        ToDolistView()
     }
 }
